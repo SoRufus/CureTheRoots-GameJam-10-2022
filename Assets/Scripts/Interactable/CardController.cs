056e3a617bcd1be552e1ea78a4124e6cc051cfad
SoRufus <wussy97@gmail.com>
2022-09-24 00:06:10 +0200

    Naprawionko kart

--- Assets/Scripts/Interactable/CardController.cs
+++ Assets/Scripts/Interactable/CardController.cs
@@ -10,18 +10,37 @@ public class CardController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI cardName = null;
     [SerializeField] private TextMeshProUGUI cardDescription = null;
 
-    public void Initialize(CardSO card)
+    private CardsDatabase cardsDatabase = null;
+
+    private void Start()
     {
+        cardsDatabase = CardsDatabase.Instance;
+
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        CardSO card = GetRandomCard();
+
         icon.sprite = card.Sprite;
-        cardName.text = card.name;
+        cardName.text = card.Name;
         cardDescription.text = card.Description;
     }
 
     private CardSO GetRandomCard()
     {
         float random = Random.value;
+        Debug.Log(random + gameObject.name);
         CardType type = random <= 0.2f ? CardType.Heal : random <= 0.55 ? CardType.Attack : CardType.Block;
 
-        return null;
+        List<CardSO> cards = new List<CardSO>();
+
+        foreach (CardSO card in cardsDatabase.Cards)
+        {
+            if(card.Type == type) cards.Add(card);
+        }
+
+        return cards[Random.Range(0, cards.Count)];
     }
 }
